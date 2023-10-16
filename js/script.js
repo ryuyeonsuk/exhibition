0187592c58838f6458342526f934930678477646
yeon <mellor7@naver.com>
2023-10-16 12:16:33 +0900

    exhibitin project ver2

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function() {
+  var swiper = new Swiper(".mainSlide", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+});
